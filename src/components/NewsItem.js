22af16408b3db5c91c4e491cd0594b09f75f2081
siwanijaiswal <siwani.jce20@sot.pdpu.ac.in>
2023-02-11 19:32:36 +0530

    changed made

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-constructor(){
-  super();
-  console.log("Hello, I am constructer");
-  this.state={
-
+  constructor() {
+    super();
+    this.state = {
+    }
   }
-}
-
   render() {
-    let {title, description,imageUrl,newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-  <img src={!imageUrl?"https://foxbaltimore.com/resources/media/0a270d88-1964-4c7a-a52e-8d4d5b95077c-large16x9_VHELOSHOCKTRAUMArender.transfer_frame_792.jpeg?1676009085175":imageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
-  </div>
-</div>
+        <div className="card">
+          <span className="position-absolute top-0
+    translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>{source}</span>
+          <img src={!imageUrl ? "https://foxbaltimore.com/resources/media/0a270d88-1964-4c7a-a52e-8d4d5b95077c-large16x9_VHELOSHOCKTRAUMArender.transfer_frame_792.jpeg?1676009085175" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-danger"> By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
       </div>
     )
   }
